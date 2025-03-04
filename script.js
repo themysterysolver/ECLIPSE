72699fe6e8c908983f527b3771523019525ab1f0
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-05 02:58:39 +0530

    FEAT:offset calculation

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let moon=document.getElementById("moon");
-moon.addEventListener("mousedown",function(event){
+const sun=document.getElementById("sun");
+const moon=document.getElementById("moon");
+const bod=document.body;
+let offsetX,offsetY;
+
+moon.addEventListener("mousedown",function(e){
+    offsetX=e.clientX-moon.offsetLeft;
+    offsetY=e.clientY-moon.offsetTop;
     document.addEventListener("mousemove",moveMoon);
     document.addEventListener("mouseup",()=>
     {
@@ -9,6 +15,17 @@ moon.addEventListener("mousedown",function(event){
 });
 
 function moveMoon(e){
-    moon.style.left=`${e.clientX}px`;
-    moon.style.top=`${e.clientY}px`;
+    let {x,y}=withinBound(e.clientX-offsetX,e.clientY-offsetY);
+    moon.style.left=`${x}px`;
+    moon.style.top=`${y}px`;
+}
+
+function withinBound(x,y){
+    let screenWidth=window.innerWidth;
+    let screenHeight=window.innerHeight;
+    let moonSize=moon.offsetWidth;
+    return {
+        x:Math.max(0,Math.min(x,screenWidth-moonSize)),
+        y:Math.max(0,Math.min(y,screenHeight-moonSize))
+    }
 }
